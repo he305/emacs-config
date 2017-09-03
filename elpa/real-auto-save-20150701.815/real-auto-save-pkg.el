0591b07ac4241b697737266663aea4b9beb8d2af
he305 <he305@mail.ru>
2017-09-03 13:09:21 +0300

    init

---
+++ elpa/real-auto-save-20150701.815/real-auto-save-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "real-auto-save" "20150701.815" "Automatically save your all your buffers/files at regular intervals." 'nil :commit "81bf61742ea553ccdee0e22d3abb43fcf07f82cf")
---
+++ init.el
@@ -0,0 +1,101 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(initial-frame-alist (quote ((fullscreen . maximized))))
+ '(package-selected-packages
+   (quote
+    (irony emmet-mode yasnippet helm dired+ python neotree org real-auto-save monokai-theme auto-complete))))
+
+;;disable backup
+(setq make-backup-files nil) ; stop creating backup~ files
+(setq auto-save-default nil) ; stop creating #autosave# files
+
+(global-hl-line-mode 1)
+(setq visible-bell 1)
+(setq frame-title-format "emacs")
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(ido-mode)
+(column-number-mode 1)
+(show-paren-mode)
+(winner-mode t)
+(windmove-default-keybindings)
+(desktop-save-mode 1)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(package-initialize)
+(ac-config-default)
+(global-auto-complete-mode t)
+
+;;autosave
+(require 'real-auto-save)
+(add-hook 'prog-mode-hook 'real-auto-save-mode)
+(setq real-auto-save-interval 60)
+
+;;font
+(set-frame-font "Consolas 14" nil t)
+
+(load-theme 'monokai t)
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;;org
+(require 'org)
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
+(setq org-agenda-files (list "C:/MEGA/todo.org"))
+(setq org-agenda-start-on-weekday 1)
+
+
+;;neotree
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
+(setq neo-theme (if (display-graphic-p) 'arrow))
+(setq neo-smart-open t)
+
+
+;;helm
+(require 'helm-config)
+(global-set-key (kbd "M-x") #'helm-M-x)
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+(helm-mode 1)
+
+;;ac-html
+(defun setup-ac-for-haml ()
+  ;; Require ac-haml since we are setup haml auto completion
+  (require 'ac-haml)
+  ;; Require default data provider if you want to use
+  (require 'ac-html-default-data-provider)
+  ;; Enable data providers,
+  ;; currently only default data provider available
+  (ac-html-enable-data-provider 'ac-html-default-data-provider)
+  ;; Let ac-haml do some setup
+  (ac-haml-setup)
+  ;; Set your ac-source
+  (setq ac-sources '(ac-source-haml-tag
+                     ac-source-haml-attr
+                     ac-source-haml-attrv))
+  ;; Enable auto complete mode
+  (auto-complete-mode))
+(add-hook 'haml-mode-hook 'setup-ac-for-haml)
+(setq sgml-quick-keys 'indent)
+
+;;c++
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
+
+(cd "C:/Git")
